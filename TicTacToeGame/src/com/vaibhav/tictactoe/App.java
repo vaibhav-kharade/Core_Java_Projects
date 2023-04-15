b9588015278bed55cb61481420098974235fcdf1
vaibhav-kharade <103056952+vaibhav-kharade@users.noreply.github.com>
2023-04-15 21:45:16 +0530

    Add files via upload

---
+++ TicTacToeGame/src/com/vaibhav/tictactoe/App.java
@@ -0,0 +1,5 @@
+package com.vaibhav.tictactoe;
+
+public class App {
+
+}
---
+++ TicTacToeGame/src/com/vaibhav/tictactoegame/LaunchGame.java
@@ -0,0 +1,91 @@
+package com.vaibhav.tictactoegame;
+
+import java.util.Iterator;
+
+class TicTacToe{
+	char[][] board;
+
+	public TicTacToe(/*char[][] board*/) {
+		board = new char[3][3];
+		initBoard();
+	}
+	
+	void initBoard() {
+		for(int i=0; i<board.length; i++) {
+			for(int j=0; j<board[i].length;j++) {
+				board[i][j]=' ';
+			}
+		}
+	}
+	
+	void dispBoard() {
+		System.out.println("-------------");
+		for(int i=0; i<board.length; i++) {
+			System.out.print("| ");
+			for(int j=0; j<board[i].length;j++) {
+				System.out.print(board[i][j]+" | ");
+			}
+			System.out.println();
+			System.out.println("-------------");
+		}
+	}
+	
+	
+	void placeMark(int row, int col, char mark) {
+		if (row>=0 && row<=2 && col>=0 && col<=2) {
+			board [row][col]=mark;
+		}
+		else {
+			System.out.println("Invalid Input or Position");
+		}
+	}
+	
+	boolean checkColWin() {
+		for(int j=0;  j<=2; j++) {
+			if (board[0][j]!=' ' && board[0][j]== board[1][j] && board[1][j]== board[2][j]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	boolean checkRowWin() {
+		for(int i=0; i<=2; i++) {
+			if (board[i][0]!=' ' && board[i][0] == board [i][1] && board[i][1] == board [i][2] ) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	boolean checkDiagWin() {
+		if (board[0][0]!=' ' && board[0][0] == board [1][1] && 
+			board[1][1] == board [2][2] || 
+					board[0][2]!=' ' && board[0][2] == board [1][1] && 
+			board[1][1] == board [2][0]) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
+
+
+
+
+
+
+
+public class LaunchGame {
+
+	public static void main(String[] args) {
+		TicTacToe t = new TicTacToe();
+				
+		t.dispBoard();
+		System.out.println(t.checkDiagWin());
+		System.out.println(t.checkRowWin());
+		System.out.println(t.checkColWin());
+	}
+}
